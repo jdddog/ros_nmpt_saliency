9cbb6ce0208be990d78c81bc32e9f20c7246115c
jdddog <jamie.diprose@gmail.com>
2014-08-25 19:26:45 +0800

    Revised the algorithm for working with 3d data, need to test with Senze3d

--- src/ros_nmpt_saliency_node.cpp
+++ src/ros_nmpt_saliency_node.cpp
@@ -19,22 +19,41 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include "geometry_msgs/Point.h"
+#include "ros_nmpt_saliency/SalientPoint2D.h"
 #include <tf/transform_broadcaster.h>
 
 
 using namespace std;
 using namespace cv;
+using namespace ros_nmpt_saliency;
 
-typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
+// Saliency tracking
 Size im_size(320,240);
 FastSalience salTracker;
 LQRPointTracker salientSpot(2);
 vector<double> lqrpt(2,.5);
 Mat image, resized_image, sal;
-ros::Publisher point_pub, image_viz_pub;
-geometry_msgs::Point point;
+
+// ROS
 int radius;
 bool visualize;
+ros::Publisher sal_2d_pub, sal_3d_pub, image_viz_pub;
+ros::Subscriber sub_pcl, sub_cam_info;
+geometry_msgs::Point point;
+
+// Depth
+sensor_msgs::PointCloud2 point_cloud_msg;
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
+sensor_msgs::CameraInfo cam_info;
+
+std::string image_topic;
+std::string point_cloud_topic;
+std::string camera_info_topic;
+bool use_depth;
+bool received_cam_info = false;
+bool received_point_cloud = false;
+
+static const double OUT_OF_RANGE_DEPTH = 3.0;
 
 
 float clamp(float value, float min, float max)
@@ -44,7 +63,7 @@ float clamp(float value, float min, float max)
 
 void update_visualization(Mat image, std_msgs::Header header, int x, int y)
 {
-    circle(image, Point(x, y), 5, CV_RGB(0,0,255));
+    circle(image, Point(x, y), 5, CV_RGB(0,0,255)); //TODO: replace with fillPoly
     cv_bridge::CvImage viz_msg;
     viz_msg.header = header;
     viz_msg.encoding = sensor_msgs::image_encodings::BGR8;
@@ -52,164 +71,157 @@ void update_visualization(Mat image, std_msgs::Header header, int x, int y)
     image_viz_pub.publish(viz_msg.toImageMsg());
 }
 
-geometry_msgs::Point get_average_point(PointCloud cloud, int x, int y, int radius)
+geometry_msgs::Point get_average_point(PointCloud cloud, int u, int v, int radius, int threshold)
 {
     geometry_msgs::Point average_point;
-    pcl::PointXYZRGB point;
+    pcl::PointXYZRGB cloud_point;
+    double fx, fy, cx, cy, depth_sum;
+    int width, height, u_start, u_end, v_start, v_end, num_valid_depths;
 
-    int width = cloud.width;
-    int height = cloud.height;
-    int x_start = clamp(x - radius, 0, width);
-    int x_end = clamp(x + radius, 0, height);
-    int y_start = clamp(y - radius, 0, height);
-    int y_end = clamp(y + radius, 0, height);
-    int cells_visited = 0;
+    // Find
+    fx = cam_info.P[0];
+    fy = cam_info.P[5];
+    cx = cam_info.P[2];
+    cy = cam_info.P[6];
+    average_point.y = -((u - cx) / fx) * OUT_OF_RANGE_DEPTH;
+    average_point.z = -((v - cy) / fy) * OUT_OF_RANGE_DEPTH;
 
-    for(int i = x_start; i < x_end; i++)
+    //Try to find depth value in range
+    width = cloud.width;
+    height = cloud.height;
+    u_start = clamp(u - radius, 0, width);
+    u_end = clamp(u + radius, 0, height);
+    v_start = clamp(v - radius, 0, height);
+    v_end = clamp(v + radius, 0, height);
+
+    depth_sum = 0.0;
+    num_valid_depths = 0;
+
+    for(int i = u_start; i < u_end; i++)
     {
-        for(int j = y_start; j < y_end; j++)
+        for(int j = v_start; j < v_end; j++)
         {
-            point = cloud.at(i, j);
-            average_point.x += point.x;
-            average_point.y += point.y;
-            average_point.z += point.z;
-            cells_visited++;tf::TransformBroadcaster br;
-            
-        }
+            cloud_point = cloud.at(i, j);
 
+            if(cloud_point.x > 0.0)
+            {
+                depth_sum += cloud_point.x;
+                num_valid_depths++;
+            }
+        }
     }
 
-    average_point.x = average_point.x / cells_visited;
-    average_point.y = average_point.y / cells_visited;
-    average_point.z = average_point.z / cells_visited;
+    if(num_valid_depths < threshold)
+    {
+        average_point.x = depth_sum / num_valid_depths;
+    }
+    else
+    {
+        average_point.x = OUT_OF_RANGE_DEPTH;
+    }
 
     return average_point;
 }
 
 void point_cloud_callback(const sensor_msgs::PointCloud2ConstPtr & msg)
 {
+    point_cloud_msg = *msg;
+    received_point_cloud = true;
+}
 
-    sensor_msgs::ImagePtr image_msg(new sensor_msgs::Image);
-
-    /* convert cloud to PCL & PCLPointCloud2 */
-    PointCloud cloud;
-    pcl::PCLPointCloud2 cloud_2;
-    pcl_conversions::toPCL(*msg, cloud_2);
-    pcl::fromPCLPointCloud2(cloud_2, cloud);
-
-
-    /* get an OpenCV image from the cloud */
-    pcl::PCLImage pcl_image;
-    pcl::toPCLPointCloud2(cloud_2, pcl_image);
-    pcl_conversions::moveFromPCL(pcl_image, *image_msg);
-
-    try
-    {
-        // Resize image
-        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
-		image = cv_ptr->image;
-
-		double ratio = im_size.width * 1. / image.cols;
-		resize(image, resized_image, Size(0,0), ratio, ratio, INTER_NEAREST);
-
-		vector<KeyPoint> pts;
-		salTracker.detect(resized_image, pts);
-		salTracker.getSalImage(sal);
-
-		double min, max;
-		Point minloc, maxloc;
-		minMaxLoc(sal, &min, &max, &minloc, &maxloc);
-
-		lqrpt[0] = maxloc.x*1.0 / sal.cols;
-		lqrpt[1] = maxloc.y*1.0 / sal.rows;
-
-		salientSpot.setTrackerTarget(lqrpt);
-		salientSpot.updateTrackerPosition();
-		lqrpt = salientSpot.getCurrentPosition();
-
-		int im_x = lqrpt[0] * resized_image.cols;
-		int im_y = lqrpt[1] * resized_image.rows;
-
-        // Get 3d point
-        //geometry_msgs::Point world_point = get_average_point(cloud, im_x, im_y, radius);
-        ROS_INFO("WIDTH: %d, HEIGHT: %d", image.cols, image.rows);
-        int b_x = im_x * (image.cols / im_size.width);
-        int b_y = im_y * (image.rows / im_size.height);
-        pcl::PointXYZRGB world_point = cloud.at(b_x, b_y);
-        ROS_INFO("scale: %f, im_x: %d, im_y: %d, b_x: %d, b_y %d, x: %f, y: %f, z: %f", ratio, im_x, im_y, b_x, b_y, world_point.x, world_point.y, world_point.z);
-
-        /*// Publish tf
-        static tf::TransformBroadcaster br;
-        tf::Transform transform;
-        transform.setOrigin(tf::Vector3(point.x, point.y, point.z));
-        transform.setRotation(tf::Quaternion(0, 0, 0, 1));
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), msg->header.frame_id, "salient_point"));
-
-        // Publish point
-        geometry_msgs::PointStamped point_stamped;
-        point_stamped.header.stamp = msg->header.stamp;
-        point_stamped.header.frame_id = msg->header.frame_id;
-        point_stamped.point = point;
-		point_pub.publish(point_stamped);*/
-
-        // Visualize
-		if(visualize)
-		{
-            update_visualization(resized_image, msg->header, im_x, im_y);
-		}
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR ("Could not convert from '%s' to 'bgr8'.", image_msg->encoding.c_str ());
-    }
+void cam_info_callback(const sensor_msgs::CameraInfoPtr& msg)
+{
+    cam_info = *msg;
+    sub_cam_info.shutdown();
+    received_cam_info = true;
 }
 
 void image_callback(const sensor_msgs::ImageConstPtr& msg)
 {
-	cv_bridge::CvImagePtr cv_ptr;
+    if(use_depth && !received_cam_info)
+    {
+        ROS_WARN("Waiting to receive CameraInfo from %s topic", camera_info_topic.c_str());
+        return;
+    }
+    else if(use_depth && received_cam_info && !received_point_cloud)
+    {
+        ROS_WARN("Waiting to receive PointCloud from %s topic", point_cloud_topic.c_str());
+        return;
+    }
 
-	try
-	{
-		cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
-		image = cv_ptr->image;
+    try
+    {
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+        image = cv_ptr->image;
 
-		double ratio = im_size.width * 1. / image.cols;
-		resize(image, resized_image, Size(0,0), ratio, ratio, INTER_NEAREST);
+        // Resize image
+        double ratio = im_size.width * 1. / image.cols;
+        resize(image, resized_image, Size(0,0), ratio, ratio, INTER_NEAREST);
 
-		vector<KeyPoint> pts;
-		salTracker.detect(resized_image, pts);
-		salTracker.getSalImage(sal);
+        // Find salient point
+        vector<KeyPoint> pts;
+        salTracker.detect(resized_image, pts);
+        salTracker.getSalImage(sal);
 
-		double min, max;
-		Point minloc, maxloc;
-		minMaxLoc(sal, &min, &max, &minloc, &maxloc);
+        double min, max;
+        Point minloc, maxloc;
+        minMaxLoc(sal, &min, &max, &minloc, &maxloc);
 
-		lqrpt[0] = maxloc.x*1.0 / sal.cols;
-		lqrpt[1] = maxloc.y*1.0 / sal.rows;
+        lqrpt[0] = maxloc.x * 1.0 / sal.cols;
+        lqrpt[1] = maxloc.y * 1.0 / sal.rows;
 
-		salientSpot.setTrackerTarget(lqrpt);
-		salientSpot.updateTrackerPosition();
-		lqrpt = salientSpot.getCurrentPosition();
+        salientSpot.setTrackerTarget(lqrpt);
+        salientSpot.updateTrackerPosition();
+        lqrpt = salientSpot.getCurrentPosition();
 
-        geometry_msgs::PointStamped point_stamped;
-        point_stamped.header.stamp = msg->header.stamp;
-        point_stamped.header.frame_id = msg->header.frame_id;
-		point_stamped.point.x = lqrpt[0] * resized_image.cols;
-		point_stamped.point.y = lqrpt[1] * resized_image.rows;
-		point_stamped.point.z = numeric_limits<float>::max();
-		point_pub.publish(point_stamped);
-		
-		ROS_INFO("x: %d, y: %d", (int)point_stamped.point.x, (int)point_stamped.point.y);
+        // Image coords in resized image
+        int im_x = lqrpt[0] * resized_image.cols;
+        int im_y = lqrpt[1] * resized_image.rows;
+
+        if(use_depth)
+        {
+            // Convert to cloud
+            PointCloud cloud;
+            pcl::PCLPointCloud2 cloud_2;
+            pcl_conversions::toPCL(point_cloud_msg, cloud_2);
+            pcl::fromPCLPointCloud2(cloud_2, cloud);
+
+            // Get image coordinates
+            double u, v;
+            u = lqrpt[0] * cloud.width;
+            v = lqrpt[1] * cloud.height;
+            int threshold = 1;
+
+            // See if there's a point in the region
+            geometry_msgs::Point point = get_average_point(cloud, u, v, radius, threshold);
+
+            geometry_msgs::PointStamped point_stamped;
+            point_stamped.header.stamp = msg->header.stamp;
+            point_stamped.header.frame_id = point_cloud_msg.header.frame_id;
+            point_stamped.point = point;
+            sal_3d_pub.publish(point_stamped);
+        }
+        else
+        {
+            SalientPoint2D point;
+            point.header.stamp = msg->header.stamp;
+            point.header.frame_id = msg->header.frame_id;
+            point.image_width = resized_image.cols;
+            point.image_height = resized_image.rows;
+            point.x = im_x;
+            point.y = im_y;
+            sal_2d_pub.publish(point);
+        }
 
         if(visualize)
         {
-		    update_visualization(resized_image, msg->header, point_stamped.point.x, point_stamped.point.y);
-		}
-	}
-	catch (Exception e)
-	{
-		ROS_ERROR("Image callback: %s", e.what());
-	}
+            update_visualization(resized_image, msg->header, im_x, im_y);
+        }
+    }
+    catch (Exception e)
+    {
+        ROS_ERROR("Image callback: %s", e.what());
+    }
 }
 
 int main(int argc, char **argv)
@@ -219,32 +231,33 @@ int main(int argc, char **argv)
 	ros::NodeHandle nhr("~");
 	image_transport::ImageTransport it(nh);
 	image_transport::Subscriber sub_image;
-	ros::Subscriber sub_pcl;
 
-
-
-	std::string image_topic;
-	std::string point_cloud_topic;
-	bool use_depth;
-
-	nhr.param<string>("image_topic", image_topic, "/usb_cam/image_raw");
-	nhr.param<string>("point_cloud_topic", point_cloud_topic, "");
+    // Load parameters
+	nhr.param<string>("image", image_topic, "/softkinetic_camera/rgb_data");
+	nhr.param<string>("point_cloud", point_cloud_topic, "");
+	nhr.param<string>("camera_info", camera_info_topic, "");
 	nhr.param<bool>("visualize", visualize, true);
 	nhr.param<bool>("use_depth", use_depth, false);
 
-	ROS_INFO("Image topic: %s", image_topic.c_str());
-	ROS_INFO("Point cloud topic: %s", point_cloud_topic.c_str());
+	ROS_INFO("Image: %s", image_topic.c_str());
+	ROS_INFO("Point cloud: %s", point_cloud_topic.c_str());
+	ROS_INFO("Camera info: %s", camera_info_topic.c_str());
 	ROS_INFO("Use depth: %s", use_depth ? "true" : "false");
 	ROS_INFO("Visualisation: %s", visualize ? "true" : "false");
 	ROS_INFO("Radius: %d", radius);
 
-	if(!use_depth)
+    // Set up publishers and subscribers
+    sub_image = it.subscribe(image_topic, 1, image_callback);
+
+	if(use_depth)
 	{
-        sub_image = it.subscribe(image_topic, 1, image_callback);
+        sub_pcl = nh.subscribe(point_cloud_topic, 1, point_cloud_callback);
+        sub_cam_info = nh.subscribe(camera_info_topic, 1, cam_info_callback);
+        sal_3d_pub = nh.advertise<geometry_msgs::PointStamped>("/salient_point_3d", 1);
 	}
 	else
 	{
-        sub_pcl = nh.subscribe(point_cloud_topic, 1, point_cloud_callback);
+	    sal_2d_pub = nh.advertise<SalientPoint2D>("/salient_point_2d", 1);
 	}
 
 	if(visualize)
@@ -252,6 +265,5 @@ int main(int argc, char **argv)
         image_viz_pub = nh.advertise<sensor_msgs::Image>("/salient_point_viz", 1);
 	}
 
-	point_pub = nh.advertise<geometry_msgs::PointStamped>("/salient_point", 50);
 	ros::spin();
 }
